51ed387a5e220b270f3c4ab88b4481cebf434ee7
Gregory Smith <gregorysmith@nostromo.land>
2021-07-30 09:47:43 -0400

    updates!

--- instances/vms/downloader/main.tf
+++ instances/vms/downloader/main.tf
@@ -1,14 +1,14 @@
 module "downloader" {
-  pm_api_url   = var.pm_api_url
-  pm_user_pass = var.pm_user_pass
-  source       = "github.com/ProfessorSalty/terraform-homelab"
-  name         = "downloader"
-  target_node  = var.proxmox_primary
-  vmid         = 506
-  clone_source = var.ubuntu_docker_template
-  os_type      = var.os_type_ubuntu
-  cores        = 2
-  memory       = 16384
+  pm_api_url      = var.pm_api_url
+  pm_user_pass    = var.pm_user_pass
+  source          = "github.com/ProfessorSalty/terraform-homelab"
+  name            = "downloader"
+  target_node     = var.proxmox_primary
+  vmid            = 506
+  clone_source    = var.ubuntu_docker_template
+  os_type         = var.os_type_ubuntu
+  cores           = 2
+  memory          = 16384
   vm_playbook_dir = var.vm_playbook_dir
 
   networks = [{
